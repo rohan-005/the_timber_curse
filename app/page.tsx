cce622eb53b5071e8604dafc9a7aeb312bd7c270
frosthowl <122034407+rohan-005@users.noreply.github.com>
2025-09-21 09:26:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,24 @@ import LinksSection from '@/components/LinksSection'
 import Footer from '@/components/Footer'
 import InteractiveTimeline from '@/components/InteractiveTimeline'
 import CursedItems from '@/components/CursedItems'
-
+import UnderConstruction from '@/components/UnderConstruction'
 export default function Home() {
   return (
     <main className="relative overflow-hidden">
       {/* <AnimatedBackground /> */}
       {/* <FloatingGhosts /> */}
       
-      <HeroSection />
+{/*       <HeroSection /> */}
+
+      <UnderConstruction/>
       
-      <div className="relative z-1">
+{/*       <div className="relative z-1">
         <AboutSection />
         <InteractiveTimeline />
         <CursedItems />
         <LinksSection />
         <Footer />
-      </div>
+      </div> */}
     </main>
   )
 }
