0c07c065fced234b2c2e5de4e492a0b50ebb9e33
frosthowl <122034407+rohan-005@users.noreply.github.com>
2025-09-21 11:28:44 +0530

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -29,7 +29,7 @@ export default function HeroSection() {
           className="w-full h-full object-cover opacity-60"
           poster="/videos/ttc_thumbnail.png"
         >
-          <source src="/videos/ttc_intro.mp4" type="video/mp4" />
+          <source src="/videos/ttc_intro11.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-80"></div>
